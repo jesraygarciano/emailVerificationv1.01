c9eb27cde5de342d7c0873d80f59afd34b4ea101
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-02 11:49:00 +0800

    Adding facades and aliases

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Jrean\UserVerification\UserVerificationServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'UserVerification' => Jrean\UserVerification\Facades\UserVerification::class,
 
     ],
 
